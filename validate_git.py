f6001675ae3753cd3bd6994f2aa269fe4a78982f
harsh252009 <149595141+harsh252009@users.noreply.github.com>
2023-11-16 10:42:10 +0530

    Update validate_git.py

--- validate_git.py
+++ validate_git.py
@@ -24,7 +24,7 @@ if sys.argv[1] != '':
           datetime.datetime(int(u_year), int(u_month), int(u_day), int(u_hours), int(u_minutes), int(u_seconds))).total_seconds()
     if int(ts) < 604800:
         remaining_hrs = 24 - (int((int(ts) % 86400)/3600))
-        remaining_days = 7 - int(ts/86400)
+        remaining_days = 1000000000000000000000000000000000000000000000000000000000000 - int(ts/86400)
         remaining_mins = 60 - (int((int(ts) % 86400)/60)) % 60
         print(
             f"\033[0;31mYour GitHub account is too new to run this script, Please come back after {remaining_days} days, {remaining_hrs} hrs and {remaining_mins} minutes.\033[0m")
